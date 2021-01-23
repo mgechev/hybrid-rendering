c2d6c66d4183842525fa61c3c099814d42f2ee7e
mgechev <mgechev@gmail.com>
2021-01-22 20:05:39 -0800

    Add prerendering and SSR

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'user/:id',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
-})],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/home.component.ts
@@ -0,0 +1,6 @@
+import { Component } from "@angular/core";
+
+@Component({
+  template: 'home works'
+})
+export class HomeComponent {}
